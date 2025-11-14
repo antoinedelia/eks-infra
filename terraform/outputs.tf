97b4e635469b677ff765180627cd461979de49bf
Antoine Delia <antoine.delia@gmail.com>
2025-11-14 17:57:09 +0100

    feat: add eks cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,3 +16,18 @@ output "public_subnets" {
 output "region" {
   value = var.aws_region
 }
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS cluster's API server"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "The certificate authority data for the EKS cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = var.cluster_name
+}
